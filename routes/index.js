357a42fa9b49a62f339d1eaf66dc32cb0db5948e
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-01-16 19:15:15 +0100

    adding more

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var projectors = require('../config/projectors')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mindokk admin' });
 });
+
+
+/* turn on projector */
+router.get('/turnOn', function(req, res, next) {
+  var proxyRequest = http.request({
+      host: '192.168.0.102',
+      port: 80,
+      method: 'POST',
+      path: '/pjctrl?D%3D%2512%2538%250%254%251%251%25114'
+    },
+    function (proxyResponse) {
+      proxyResponse.on('data', function (chunk) {
+        res.send(chunk);
+      });
+    });
+
+  // proxyRequest.write(res.body);
+  // proxyRequest.end();
+  res.render('index', { title: 'OK' });
+});
+
 
 module.exports = router;
