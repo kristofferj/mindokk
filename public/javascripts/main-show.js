c5b647b8d4a5328fceb096be29b9b429dac66f19
Kristoffer Sivertsen <kristofferjoachim@gmail.com>
2016-01-23 18:37:32 +0100

    fullscreen

--- public/javascripts/main-show.js
+++ public/javascripts/main-show.js
@@ -1,11 +1,27 @@
+var currentItem = 0;
+var numberOfItems = 0;
+
+function show(item) {
+  $('ul.items li').hide();
+  $(item).show();
+  nextItem(item);
+}
+function nextItem(item) {
+
+  var item = $('ul.items li')[currentItem];
+  var time = $(item).data('time');
+  console.log(time, item);
+  if (currentItem < numberOfItems) {
+    currentItem++;
+    //setTimeout(function(){ show($('ul.items li')[currentItem])}, time * 1000);
+
+  } else {
+    location.reload();
+  }
+
+}
+
 $(document).ready( function() {
-
-  $('ul.items li').each( function(i, item) {
-    console.log('time', $(item).data('time'));
-    if (i == 0) {
-      console.log('Showing first');
-      $(item).show();
-    }
-  })
-
+  numerOfItems = $('ul.items li').length;
+  $('ul.items li').first().show();
 });
